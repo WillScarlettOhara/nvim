3dbe266cff57633e1f6905345870403ac995baa4
WillScarlettOhara <leilumultipass@gmail.com>
2026-03-20 23:05:35 +0100

    replace mini.statusline with lualine to show buffers opened

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -38,16 +38,6 @@ return {
       require("mini.surround").setup()
 
       require("mini.pairs").setup()
-
-      local statusline = require("mini.statusline")
-      statusline.setup({
-        use_icons = vim.g.have_nerd_font,
-        set_vim_settings = false,
-      })
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return "%2l:%-2v %P"
-      end
     end,
   },
 
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,3 +1,49 @@
+-- define colors
+local colors = {
+  gruvBlue = "#83a598",
+  gruvGreen = "#8ec07c",
+  gruvPink = "#d3869b",
+  gruvYellow = "#fabd2f",
+  gruvRed = "#fb4934",
+  gruvWhite = "#ebdbb2",
+  gruvBlack = "#1d2021",
+  gruvGray = "#3c3836",
+  gruvDark = "#282828",
+}
+
+-- custom modifications
+local gruv_material = {
+  normal = {
+    a = { bg = colors.gruvDark, fg = colors.gruvWhite, gui = "bold" },
+    b = { bg = colors.gruvGray, fg = colors.gruvWhite, gui = "bold" },
+    c = { bg = colors.gruvBlue, fg = colors.gruvBlack, gui = "bold" },
+  },
+  insert = {
+    a = { bg = colors.gruvBlue, fg = colors.gruvBlack, gui = "bold" },
+    c = { bg = colors.gruvPink, fg = colors.gruvBlack, gui = "bold" },
+  },
+  visual = {
+    a = { bg = colors.gruvPink, fg = colors.gruvBlack, gui = "bold" },
+    c = { bg = colors.gruvDark, fg = colors.gruvWhite, gui = "bold" },
+  },
+  command = {
+    a = { bg = colors.gruvGreen, fg = colors.gruvBlack, gui = "bold" },
+    c = { bg = colors.gruvBlack, fg = colors.gruvWhite, gui = "bold" },
+  },
+  terminal = {
+    a = { bg = colors.gruvRed, fg = colors.gruvBlack, gui = "bold" },
+    c = { bg = colors.gruvBlack, fg = colors.gruvWhite, gui = "bold" },
+  },
+  replace = {
+    a = { bg = colors.gruvBlue, fg = colors.gruvBlack, gui = "bold" },
+    c = { bg = colors.gruvPink, fg = colors.gruvBlack, gui = "bold" },
+  },
+  inactive = {
+    a = { bg = colors.gruvGreen, fg = colors.gruvBlack, gui = "bold" },
+    c = { bg = colors.gruvBlack, fg = colors.gruvWhite, gui = "bold" },
+  },
+}
+
 -- UI: Which-key, diagnostics display, notifications, and visual enhancements
 return {
   -- ════════════════════════════════════════════════════════════════════════════
@@ -301,4 +347,67 @@ return {
   -- nvim-web-devicons
   -- ════════════════════════════════════════════════════════════════════════════
   { "nvim-tree/nvim-web-devicons", opts = {} },
+  -- ════════════════════════════════════════════════════════════════════════════
+  -- lualine
+  -- ════════════════════════════════════════════════════════════════════════════
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = { "VeryLazy" },
+    opts = {
+      options = {
+        theme = gruv_material,
+        component_separators = { left = "|", right = "|" },
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = { "snacks_dashboard" },
+      },
+      sections = {
+        lualine_a = {
+          "mode",
+        },
+        lualine_b = {
+          "branch",
+          "diff",
+          "diagnostics",
+          {
+            "buffers",
+            buffers_color = {
+              active = { bg = colors.gruvYellow, fg = colors.gruvBlack, gui = "bold" },
+              inactive = { bg = colors.gruvGray, fg = colors.gruvWhite, gui = "italic" },
+            },
+            symbols = {
+              modified = " ●",
+              alternate_file = "",
+              directory = "",
+            },
+            mode = 2,
+          },
+        },
+        lualine_c = {
+          {
+            "filename",
+            file_status = true,
+            path = 3,
+          },
+        },
+        lualine_x = {
+          "filesize",
+        },
+        lualine_y = {
+          "searchcount",
+          "selectioncount",
+          "lsp_status",
+          "filetype",
+        },
+        lualine_z = {
+          "encoding",
+          "location",
+        },
+      },
+    },
+    config = function(_, opts)
+      require("lualine").setup(opts)
+      vim.opt.laststatus = 3
+    end,
+  },
 }
